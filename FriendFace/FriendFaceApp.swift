344014e33f03d7ffc1d3428f3468389eac183518
fernandocallejasr <ferd.callejas@gmail.com>
2024-08-15 21:34:20 -0600

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Fernando Callejas on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
